3bd72b822be5f4dce0636982506a1ce213fb1202
nimeshaherath <ghost-of-uchiha@hotmail.com>
2023-08-22 20:25:39 +0530

    Added the Navbar

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+//Importing all the components inside the components folder
+
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  CTA,
+  Clients,
+  Footer,
+  Hero,
+  Stats,
+  Testimonials,
+};
